96d85d356d80c6d73adf7fc5213f43f7f05c4165
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-09-15 18:42:34 -0400

    added exports back in

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import "./App.css";
 import { API, Storage } from "aws-amplify";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation,} from "./graphql/mutations";
 
 const initialFormState = { name: "", description: "" };
 
